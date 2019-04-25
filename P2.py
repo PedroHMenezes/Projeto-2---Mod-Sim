63af4ceee153139af34228995fde75a764f25058
PedroHMenezes <49311416+PedroHMenezes@users.noreply.github.com>
2019-04-25 08:35:33 -0300

    Ajustando equações

--- P2.py
+++ P2.py
@@ -9,31 +9,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 #lista tempo
-delta_t=0.01
-tempo=np.arange(0,30,delta_t)
+delta_t=0.001
+tempo=np.arange(0,1,delta_t)
 #variáveis
 m=152
 e=0.95
 sigma=5.67e-8
 A=106.56e-4
-c=0.22
-k=205
+c=0.72
+k=0.8
 h=10
 d=2.5e-2
 Tsol=5778
+P=3.94
 #lista_condição_inicial
-lista_condicao=[293]
+lista_condicao=[300,293]
 #funcao_dUdt
 def Var_T(listaT,t):
-    T=listaT[0]
-    P=3.94*t
-    U=m*c*T
+    Tcel=listaT[0]
+    Tamb=listaT[1]
     I=e*sigma*A*Tsol**4
-    Cond=U*k*A/d
-    Conv=U*h*A
-    dTdt=(P+I-(Cond+Conv))/(m*c)
-    T=T+dTdt
-    return T
+    Qdis=P*delta_t
+    Conv=h*A*(Tamb-Tcel)
+    CondConv= (Tamb-Tcel)/((1/(h*A))+(d/(k*A)))
+    dTdt=((I+Qdis-Conv-CondConv)/(m*c))*delta_t
+    Tcel=Tcel+dTdt
+    return Tcel,Tamb
+
 #grafico_temperatura
 lista_grafico=odeint(Var_T,lista_condicao,tempo)
 print(lista_grafico[:])
